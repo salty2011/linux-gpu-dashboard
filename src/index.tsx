87435f8b4c51c28a58acba15cdd7f0b5ba515545
salty2011 <salty2011@proton.me>
2025-03-21 15:09:32 +1100

    Corecting build errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LinuxGPUDashboard from '../linux-gpu-dashboard';
+import LinuxGPUDashboard from './linux-gpu-dashboard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
